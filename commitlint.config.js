75991bdbe66873cc4e820aca82a3cc3258510e02
r-falcon <rfalcon@163.com>
2022-02-17 16:57:05 +0800

    fix: files update

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  // extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'type-enum': [
-  //     2,
-  //     'always',
-  //     ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']
-  //   ],
-  //   'subject-full-stop': [0, 'never'],
-  //   'subject-case': [0, 'never']
-  // }
-  extends: ['cz'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    // 自定义规则
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
   }
 }
